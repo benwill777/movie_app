9ceaae51d0dd8704b4bacbaeb00936e42e424043
benwill777 <t.bennettwilliamsjr@gmail.com>
2018-05-17 08:56:49 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'movies/index'
-  get 'movies/show'
-  get 'movies/delete'
-  get 'movies/update'
-  get 'movies/create'
+  get 'movies#index'
+  get 'movies#show'
+  get 'movies#delete'
+  get 'movies#update'
+  get 'movies#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
